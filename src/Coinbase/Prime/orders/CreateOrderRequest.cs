90ed4d0b5ddc474c2e3928204c667cb691c52ce0
Nick Morgan <nick.morgan@coinbase.com>
2024-07-26 07:52:12 -0700

    update order apis

---
+++ src/Coinbase/Prime/orders/CreateOrderRequest.cs
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2024-present Coinbase Global, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+using System;
+using Newtonsoft.Json;
+
+namespace Coinbase.Prime.Orders
+{
+  public class CreateOrderRequest
+  {
+    [JsonProperty("portfolio_id", Required = Required.Always)]
+    public string PortfolioId { get; set; }
+
+    [JsonProperty("product_id")]
+    public string ProductId { get; set; }
+
+    public OrderSide Side { get; set; }
+
+    [JsonProperty("client_order_id")]
+    public string ClientOrderId { get; set; }
+
+    public OrderType Type { get; set; }
+
+    [JsonProperty("base_quantity")]
+    public string BaseQuantity { get; set; }
+
+    [JsonProperty("quote_value")]
+    public string QuoteValue { get; set; }
+
+    [JsonProperty("limit_price")]
+    public string LimitPrice { get; set; }
+
+    [JsonProperty("stop_price")]
+    public string StopPrice { get; set; }
+
+    [JsonProperty("time_in_force")]
+    public TimeInForce TimeInForce { get; set; }
+
+    [JsonProperty("start_time")]
+    public string StartTime { get; set; }
+
+    [JsonProperty("expiry_time")]
+    public string ExpiryTime { get; set; }
+
+    [JsonProperty("stp_id")]
+    public string StpId { get; set; }
+
+    [JsonProperty("display_quote_size")]
+    public string DisplayQuoteSize { get; set; }
+
+    [JsonProperty("display_base_size")]
+    public string DisplayBaseSize { get; set; }
+
+    [JsonProperty("is_raise_exact")]
+    public bool IsRaiseExact { get; set; }
+
+    [JsonProperty("historical_pov")]
+    public string HistoricalPov { get; set; }
+
+    public CreateOrderRequest() { }
+  }
+}
